cfa64d99c56752aa3436b2d3ae796d5ad9e29fcf
Facnet <zabuza53@yandex.ru>
2022-07-23 13:23:40 +0300

    23072022 убрал лишний import

--- src/Task_one/begin.java
+++ src/Task_one/begin.java
@@ -1,10 +1,5 @@
 package Task_one;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-
 public class begin {
     public static void main(String[] args) {
         /*
